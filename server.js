4c475c97bceb4f8f1f988a147a74e4f640403bfb
rodcko2417 <rodcko@live.com>
2020-03-29 21:52:59 -0600

    Recibir información desde el cliente: Body y Query

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+// Body Parser es una extension/modulo de Express, que nos permite trabajar con el body de la peticion de forma sencilla
+// npm i body parser
+const bodyParser = require('body-parser');
+
+
 // Router de Express
 const router = express.Router(); // Nos permite separar cabeceras, por metodo, url, etc
 
@@ -7,15 +12,20 @@ const router = express.Router(); // Nos permite separar cabeceras, por metodo, u
 // import express from 'express'
 
 var app = express(); // Con esto ya inicializamos express
-
+app.use(bodyParser.json()); // Agregamos bodyParser
+app.use(bodyParser.urlencoded({extended: false})); // Podemos cambiar el metodo que querramos llamar, en este caso urlencoded
 app.use(router); // Agregamos el router
 
 router.get('/message', function (req, res) { // Solo atiendo a las peticiones get
-    res.send('Lista de mensajes');
+    // Aca va el body, en la request
+    console.log(req.body);
+    // O bien, podemos acceder al query así 
+    console.log(req.query);
+    res.send('Mensaje ' + req.body.text  + ' añadido correctamente'); // Aca mandamos el body al cliente
 });
 
 router.post('/message', function (req, res) { // Solo atiendo a las peticiones post
-    res.send('Mensaje añadido');
+    res.send('Mensaje añadido correctamente xd');
 });
 
 // Con el router ya podemos separar por rutas y los diferentes metodos HTTP
@@ -26,3 +36,5 @@ router.post('/message', function (req, res) { // Solo atiendo a las peticiones p
 
 app.listen(3000);
 console.log('La aplicación está escuchando en http://localhost:3000')
+
+// *** Instalar nodemon, para hacer cambios y no tener que tumbar el server npm install nodemon
