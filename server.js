e79fdbc2cd76d0cd59122bba0ff5dbc71ffff4b6
rodcko2417 <rodcko@live.com>
2020-04-07 11:43:44 -0600

    Se agrega el frontend para hacer pruebas y se habilitan las cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express'); // Otra manera de escribirlo: import express
 const app = express(); // Con esto ya inicializamos express
 const server = require('http').Server(app);
 
+const cors = require('cors'); // Traemos cors
 const bodyParser = require('body-parser'); // Body Parser es una extension/modulo de Express, que nos permite trabajar con el body de la peticion de forma sencilla
 const socket = require('./socket');
 const db = require('./db');
@@ -10,6 +11,8 @@ const db = require('./db');
 const router = require('./network/routes');
 
 db('mongodb+srv://rodcko:rodcko123@cluster0-kgypc.mongodb.net/test');
+
+app.use(cors());
 
 app.use(bodyParser.json()); // Agregamos bodyParser
 app.use(bodyParser.urlencoded({extended: false})); // Podemos cambiar el metodo que querramos llamar, en este caso urlencoded
@@ -20,8 +23,11 @@ socket.connect(server);
 router(app); // Le pasamos nuestras rutas
 
 // Servidor estático de archivos
-app.use('/app', express.static('public')); 
+app.use('/', express.static('public')); 
 
 server.listen(3000, function () {
     console.log('La aplicación está escuchando en http://localhost:3000')
 }); 
+
+
+// Habilitar las cabeceras con el modulo CORS instalacion: npm i cors
